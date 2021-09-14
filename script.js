ab341895c7a5700d0fed170cb6cc233da838e3a1
jaredatandi <jaredatandi07@gmail.com>
2021-09-14 09:37:08 +0300

    add js variables

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function birthDay(){
     var date = document.getElementById('date').value;
     var month = document.getElementById('month').value;
-    var year = = document.getElementById('date').value;
+    var year = document.getElementById('date').value;
 
-    
+
     
 }
