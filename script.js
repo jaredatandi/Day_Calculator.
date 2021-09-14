6bee27bd633925f3bd522dc30ca75e5f2321346e
jaredatandi <jaredatandi07@gmail.com>
2021-09-14 09:42:03 +0300

    create array for matching name and days

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 function birthDay(){
     var date = document.getElementById('date').value;
     var month = document.getElementById('month').value;
-    var year = document.getElementById('date').value;
+    var year = document.getElementById('year').value;
 
+    var century = year.slice(0,2);
+    var DOB = year.slice(2,4);
 
+    var userGender = document.getElementById("gender");
+    var gender = userGender.options[userGender.selectedIndex].text;
+
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var female = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+    var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
     
 }
